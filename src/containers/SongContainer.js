4148ced314a999a92a8177f3d5434f09a1468428
lauren brett <laurencareybrett@gmail.com>
2020-03-03 16:47:22 +0000

    set up itunes

---
+++ src/containers/SongContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class SongContainer extends Component {
+
+
+    render(){
+        return(
+            <h1>Song Container</h1>
+        )
+    }
+}
+
+export default SongContainer;
